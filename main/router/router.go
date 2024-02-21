f7abb73cc3c423197ea2d8592c1e19d773aa6d45
neongenesis <909818389@qq.com>
2024-02-21 23:14:03 +0800

    upload路由使用文件校验中间件

--- main/router/router.go
+++ main/router/router.go
@@ -35,7 +35,7 @@ func (app AppRouter) userRouter() {
 func (app AppRouter) fileRouter() {
 	router := app.Engine.Group("/files", middleware.AuthJWT)
 	{
-		router.POST("/upload", app.FileApi.UploadFunc)
+		router.POST("/upload", middleware.ValidFormFile, app.FileApi.UploadFunc)
 		router.GET("/download", app.FileApi.DownloadFunc)
 	}
 }
